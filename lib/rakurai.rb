ad05af06a86a53030e82e01b4974ab0619722d45
Naoto SHINGAKI <nao.sora+github@gmail.com>
2012-11-08 18:01:05 +0900

    set server ip

--- lib/rakurai.rb
+++ lib/rakurai.rb
@@ -10,10 +10,25 @@ module Rakurai
     config = Rakurai::Config.load("#{File.dirname(__FILE__)}/../config.yaml")
     @server = Rack::Server.new(
       server: :webrick,
-      Host: "localhost",
+      Host: local_ip,
       Port: config.port,
       app: Rakurai::Server.new(config)
     )
     @server.start
   end
+
+  private
+   # networking - Getting the Hostname or IP in Ruby on Rails - Stack Overflow
+   # http://stackoverflow.com/questions/42566/getting-the-hostname-or-ip-in-ruby-on-rails
+   def self.local_ip
+     # turn off reverse DNS resolution temporarily
+     orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+
+     UDPSocket.open do |s|
+       s.connect('8.8.8.8', 1)
+       s.addr.last
+     end
+   ensure
+     Socket.do_not_reverse_lookup = orig
+   end
 end
